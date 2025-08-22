0391162c163ef6fa41e160a9e1c926b510245042
itai <itai@itai.ai>
2025-08-22 15:31:12 +0200

    added the Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import './App.css'
 import Converter from './components/Converter'
+import Header from './components/Header'
 
 const queryClient = new QueryClient()
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
+        <Header />
         <Converter />
       </QueryClientProvider>
     </>
